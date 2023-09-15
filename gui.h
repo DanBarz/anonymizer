c83246bcdfb95be64a77a54ebda24cdd20b0be6d
Daniel Allen <danielpeterallen@gmail.com>
2023-09-15 13:49:43 +1000

    Working but not ideal

---
+++ gui.h
@@ -0,0 +1,16 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include "json.hpp"
+#include <string>
+
+struct GUI {
+    std::string title;
+    int width;
+    int height;
+};
+
+void init_gui(GUI &gui, const std::string &configFile);
+void run_gui(GUI &gui);
+
+#endif // GUI_H
--- program.cpp
+++ program.cpp
@@ -1,5 +1,6 @@
 
 #include "validation.h"
+#include "gui.h"
 #include "anonymizer.h"
 #include "email_processing.h"
 #include <vector>
@@ -10,6 +11,11 @@ int main() {
     placeholder_data placeholders;
     initialize_patterns();
     validate_email_data(placeholders);
+
+    
+    GUI myGui;
+    init_gui(myGui, "gui_config.json");
+    run_gui(myGui);
 
     return 0;
 }
